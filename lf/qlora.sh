c6debfe74b6f57b79fb0d21e152f881ab945a55c
Canbin Huang <huangcb01@foxmail.com>
2024-12-20 00:27:53 +0800

    add vllm tester

--- lf/qlora.sh
+++ lf/qlora.sh
@@ -19,6 +19,8 @@ TEMPLATE=llama2
 # MODEL_PATH=meta-llama/Llama-3.1-8B
 # MODEL_PATH=Qwen/Qwen2.5-7B
 # MODEL_PATH=google/gemma-2-9b
+LORA_TARGET=all
+LORA_RANK=16
 
 DATA_DIR=../processed_data
 DATASET=iemocap
@@ -33,10 +35,12 @@ DATA_PERCENT=1.0
 PER_DEVICE_TRAIN_BATCH_SIZE=32
 GRADIENT_ACCUMULATION_STEPS=1
 LEARNING_RATE=2e-4
+LR_SCHEDULER_TYPE=cosine
 NUM_TRAIN_EPOCHS=6
 ADDITIONAL_ARGS="--use_unsloth"
 
 # Process data
+echo "Processing data..."
 python ../code/data_process_ours.py \
     --dataset ${DATASET} \
     --historical_window ${HISTORICAL_WINDOW} \
@@ -44,12 +48,13 @@ python ../code/data_process_ours.py \
     --bio ${BIO}
 
 # Training
-DATASET=${DATASET}_${COT_TYPE}_bio${BIO}
+echo "Training..."
+TRAIN_DATASET=${DATASET}_${COT_TYPE}_bio${BIO}
 SCRIPT_NAME=$(basename "$0")
 MODEL_NAME=$(basename $MODEL_PATH)
 CUDAS=$(echo "$CUDA_VISIBLE_DEVICES" | tr ',' '\n' | wc -l)
 TOTAL_BATCH_SIZE=$((PER_DEVICE_TRAIN_BATCH_SIZE * GRADIENT_ACCUMULATION_STEPS * CUDAS))
-OUTPUT_DIR=output/${MODEL_NAME}/${DATASET}/full_bs-${TOTAL_BATCH_SIZE}_lr-${LEARNING_RATE}_epochs-${NUM_TRAIN_EPOCHS}
+OUTPUT_DIR=output/${MODEL_NAME}/${DATASET}/${VERSION}/qlora-${LORA_TARGET}-${LORA_RANK}_bs-${TOTAL_BATCH_SIZE}_lr-${LEARNING_RATE}-${LR_SCHEDULER_TYPE}_epochs-${NUM_TRAIN_EPOCHS}
 mkdir -p $OUTPUT_DIR
 cat $SCRIPT_NAME > $OUTPUT_DIR/train.sh
 llamafactory-cli train \
@@ -63,8 +68,8 @@ llamafactory-cli train \
     --lora_target all \
     --lora_rank 16 \
     --dataset_dir $DATA_DIR \
-    --dataset ${DATASET}_train \
-    --eval_dataset ${DATASET}_valid \
+    --dataset ${TRAIN_DATASET}_train \
+    --eval_dataset ${TRAIN_DATASET}_valid \
     --template $TEMPLATE \
     --cutoff_len 4096 \
     --preprocessing_num_workers 32 \
@@ -76,39 +81,40 @@ llamafactory-cli train \
     --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS \
     --learning_rate $LEARNING_RATE \
     --num_train_epochs $NUM_TRAIN_EPOCHS \
-    --lr_scheduler_type cosine \
+    --lr_scheduler_type $LR_SCHEDULER_TYPE \
     --warmup_ratio 0.1 \
     --bf16 \
     $ADDITIONAL_ARGS \
     | tee $OUTPUT_DIR/train.log
 
+# Validation
+echo "Validation..."
+for checkpoint in ${OUTPUT_DIR}/checkpoint-*/; do
+    python test.py \
+        --model_name_or_path $MODEL_PATH \
+        --dataset ${DATASET} \
+        --data_path ${DATA_DIR}/${DATASET}/${COT_TYPE}_bio${BIO}/valid.json$ \
+        --output_dir ${checkpoint}/valid \
+        --use_chat_template False \
+        --quantization bitsandbytes \
+        --qlora_adapter_name_or_path $checkpoint \
+        --load_format bitsandbytes \
+        --enable_lora True \
+        --max_lora_rank $LORA_RANK
 
 
-
-OUTPUT_DIR=/nas-wulanchabu/hongzhan.chz/tmp/self-improvement/output/eft/Meta-Llama-3-8B-Instruct/oasst1_first-turn_en_train_judged_gpt-4o_eft_all/full_bs-32_lr-5e-8_epochs-10
-PROMPT_PATH="prompts/llm_as_a_judge_prompt.txt"
-INPUT_PATH="data/oasst1/first-turn_en_validation.json"
-TEMPERATURE=0.8
-TOP_P=0.95
-
-
-for model in ${OUTPUT_DIR}/*/; do
-    output_path="${model}eval/judgements.json"
-    metric_path="${model}eval/metrics.json"
-    echo $model
-    # echo "Generating judgements"
-    # python src/judge.py vllm \
-    #     --model_path $model \
-    #     --dtype bfloat16 \
-    #     --prompt_path $PROMPT_PATH \
-    #     --input_path $INPUT_PATH \
-    #     --output_path $output_path \
-    #     --n_judgements_per_response 11 \
-    #     --temperature $TEMPERATURE \
-    #     --top_p $TOP_P
-
-    echo "Evaluating judgements"
-    python src/evaluate.py judgement \
-        --judgement_path $output_path \
-        --output_path $metric_path
+# Test
+echo "Test..."
+for checkpoint in ${OUTPUT_DIR}/checkpoint-*/; do
+    python test.py \
+        --model_name_or_path $MODEL_PATH \
+        --dataset ${DATASET} \
+        --data_path ${DATA_DIR}/${DATASET}/${COT_TYPE}_bio${BIO}/test.json$ \
+        --output_dir ${checkpoint}/test \
+        --use_chat_template False \
+        --quantization bitsandbytes \
+        --qlora_adapter_name_or_path $checkpoint \
+        --load_format bitsandbytes \
+        --enable_lora True \
+        --max_lora_rank $LORA_RANK
 done
--- lf/test.py
+++ lf/test.py
@@ -1,30 +1,48 @@
+import os
 import fire
 import json
+import torch
+from typing import Literal
 from vllm import LLM, SamplingParams
+from sklearn import metrics
+from sklearn.metrics import accuracy_score, f1_score
 
-prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
-]
-sampling_params = SamplingParams(temperature=0.8, top_p=0)
 
-llm = LLM(model="facebook/opt-125m")
+LABELS = {
+    "iemocap": ["happy", "sad", "neutral", "angry", "excited", "frustrated"],
+    "meld": ["neutral", "surprise", "fear", "sad", "joyful", "disgust", "angry"],
+    "EmoryNLP": ["Joyful", "Mad", "Peaceful", "Neutral", "Sad", "Powerful", "Scared"],
+    "dialydailog": ["happy", "neutral", "angry", "sad", "fear", "surprise", "disgust"],
+}
+LABEL2ID = {dataset: {label: i for i, label in enumerate(labels)} for dataset, labels in LABELS.items()}
 
-outputs = llm.generate(prompts, sampling_params)
 
-for output in outputs:
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    print(f"Prompt: {prompt!r}, Generated text: {generat
+def main(
+    model_name_or_path: str,
+    dataset: Literal["iemocap", "meld", "EmoryNLP"],
+    data_path: str,
+    output_dir: str,
+    use_chat_template: bool = False,
+    max_tokens: int = 1024,
+    temperature: float = 1,
+    top_p: float = 1,
+    top_k: int = -1,
+    **kwargs,
+):
+    """ERC Tester.
 
-def main(dataset: str, data_path: str, model_path: str, adapter_path: str | None = None):
-    # Load data
-    with open(data_path, "r", encoding="utf-8") as f:
-        data = json.load(f)
-    prompts = [item["input"] for item in data]
-
+    Args:
+        model_name_or_path: Name or path of the model.
+        dataset: Dataset name chosen from ["iemocap", "meld", "EmoryNLP"].
+        data_path: Path to the dataset file.
+        output_path: Path to the output file.
+        use_chat_template: Whether to use chat template.
+        max_tokens: Maximum tokens to generate.
+        temperature: Temperature for sampling.
+        top_p: Top-p for sampling.
+        top_k: Top-k for sampling.
+        **kwargs: Other keyword arguments for the `vllm.LLM` class.
+    """
     # Load model
     llm_kwargs = {
         "dtype": "float16",
@@ -43,7 +61,132 @@ def main(dataset: str, data_path: str, model_path: str, adapter_path: str | None
         trust_remote_code=True,
         **llm_kwargs,
     )
-    self.tokenizer = self.llm.get_tokenizer()
+
+    # Load data
+    with open(data_path, "r", encoding="utf-8") as f:
+        data: list[dict[str, str]] = json.load(f)
+
+    # Inference
+    sampling_params = SamplingParams(temperature=temperature, top_p=top_p, top_k=top_k)
+    if use_chat_template:
+        messages = [[{"role": "user", "content": item["input"]}] for item in data]
+        outputs = llm.chat(messages, sampling_params=sampling_params)  # type: ignore
+    else:
+        prompts = [item["input"] for item in data]
+        outputs = llm.generate(prompts, sampling_params=sampling_params)
+    outputs = [output.outputs[0].text for output in outputs]
+
+    # Save results
+    os.makedirs(output_dir, exist_ok=True)
+    with open(os.path.join(output_dir, "predictions.json"), "w", encoding="utf-8") as f:
+        json.dump(
+            [
+                {"input": item["input"], "output": output, "target": item["target"]}
+                for item, output in zip(data, outputs)
+            ],
+            f,
+            ensure_ascii=False,
+            indent=4,
+        )
+
+    # Compute metrics
+    label2id = LABEL2ID[dataset]
+    predictions, labels = [], []
+    confused_cases = []
+    for i, (item, output) in enumerate(zip(data, outputs)):
+        label = item["target"].strip().split("\n")[-1].strip()
+        labels.append(label2id[label])
+        prediction = output.strip().split("\n")[-1].strip()
+        match_result = _match_text(prediction, LABELS[dataset])
+        if match_result:
+            predictions.append(label2id[match_result[0]])
+        else:
+            predictions.append(label2id[_optimize_output(prediction, LABELS[dataset])])
+            confused_cases.append(i)
+    assert len(predictions) == len(labels)
+    score, res_matrix = report_score(dataset, labels, predictions)
+
+    # Save metrics
+    with open(os.path.join(output_dir, "metrics.json"), "w", encoding="utf-8") as f:
+        f.write(json.dumps(score))
+        f.write(f"\n{res_matrix}")
+        f.write(f"\nconfuse_case:{confused_cases}\n")
+
+
+def report_score(dataset: str, golds: list[str], preds: list[str]):
+    if dataset == "iemocap":
+        target_names = ["hap", "sad", "neu", "ang", "exc", "fru"]
+        digits = 6
+    elif dataset == "meld":
+        target_names = ["neutral", "surprise", "fear", "sad", "joyful", "disgust", "angry"]
+        digits = 7
+    elif dataset == "EmoryNLP":
+        target_names = ["Joyful", "Mad", "Peaceful", "Neutral", "Sad", "Powerful", "Scared"]
+        digits = 7
+
+    res = {}
+    res["Acc_SA"] = accuracy_score(golds, preds)
+    res["F1_SA"] = f1_score(golds, preds, average="weighted")
+    for k, v in res.items():
+        if isinstance(v, float):
+            res[k] = round(v * 100, 3)
+    res_matrix = metrics.classification_report(golds, preds, target_names=target_names, digits=digits)
+    return res, res_matrix
+
+
+def _match_text(text, word_set_):
+    if text is None:
+        return []
+    len_text = len(text)
+    s_idx = 0
+    match_res = []
+    while s_idx < len_text:
+        cache = []
+        span_length = 1
+        while span_length < 12 and s_idx + span_length <= len_text:
+            span = text[s_idx : s_idx + span_length]
+            if span in word_set_:
+                cache.append(span)
+            span_length += 1
+        if len(cache) > 0:
+            match_res.append(cache[-1])
+            s_idx += len(cache[-1])
+        else:
+            s_idx += 1
+    return match_res
+
+
+def _optimize_output(output, label_set):
+    """
+    Calculate output and label_ Set the editing distance of each label in the set and return the label corresponding to the minimum editing distance
+    """
+    min_distance = float("inf")
+    optimized_output = ""
+    for label in label_set:
+        distance = _edit_distance(output, label)
+        if distance < min_distance:
+            min_distance = distance
+            optimized_output = label
+    return optimized_output
+
+
+def _edit_distance(s1, s2):
+    """
+    Calculate the editing distance between two strings
+    """
+    m, n = len(s1), len(s2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(m + 1):
+        dp[i][0] = i
+    for j in range(n + 1):
+        dp[0][j] = j
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+    return dp[m][n]
 
 
 if __name__ == "__main__":
